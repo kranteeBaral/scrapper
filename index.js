6b0b477eca0126bdfecf57b2f6826a6bb3460601
kranteeBaral <krantee.baral@bandipurcablecar.com.np>
2024-05-16 13:08:17 +0545

    server.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const PORT = 8000;
-const URL = "https://www.theguardian.com/uk";
+const URL = "https://www.google.com/maps/search/hotels+in+pokhara/@28.207006,83.921482,13z/data=!4m2!2m1!6e3?entry=ttu";
 
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -7,20 +7,32 @@ const express = require('express')
 
 const app = express()
 
-axios(URL).then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    // console.log(html);
+
+// axios(URL).then(response => {
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     console.log(html);
+//     const listOfHotels = [];
+//     $('.TFQHme', html).each(function() {
+//         // const name = $(this).find('a').attr('aria-label');
+//         const detail = $(this).find('a').attr('href');
+//         listOfHotels.push({ detail})
+//     })
+//     console.log('The list of hotels ', listOfHotels);
+
+
     // $('.dcr-jfeohd').attr(href)
-    const newsArray = [];
-    $('.dcr-16c50tn', html).each(function(){
-        const newsTitle = $(this).text();
-        const link = $(this).find('a').attr('href');
-        newsArray.push({newsTitle, link})
-    })
-    console.log(newsArray);   
-}).catch(error => console.log(`Error occurred: ${error}`))
+    // const newsArray = [];
+    // $('.dcr-16c50tn', html).each(function(){
+    //     const newsTitle = $(this).text();
+    //     const link = $(this).find('a').attr('href');
+    //     newsArray.push({newsTitle, link})
+    // })
+    // console.log(newsArray);   
+
+
+// }).catch(error => console.log(`Error occurred: ${error}`))
 
 app.listen(PORT, () => {console.log(`Server running on PORT: ${PORT}`)})
 
-// anchor class dcr-lv2v9o
+
