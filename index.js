e4fa897d373c495586e6757d7e53790f7c6ac005
kranteeBaral <krantee.baral@bandipurcablecar.com.np>
2024-05-16 11:21:14 +0545

    newsTitle and link scrapped off

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const PORT = 8000;
+const URL = "https://www.theguardian.com/uk";
 
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 
 const app = express()
-app.listen(PORT)
+
+axios(URL).then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    // console.log(html);
+    // $('.dcr-jfeohd').attr(href)
+    const newsArray = [];
+    $('.dcr-16c50tn', html).each(function(){
+        const newsTitle = $(this).text();
+        const link = $(this).find('a').attr('href');
+        newsArray.push({newsTitle, link})
+    })
+    console.log(newsArray);   
+}).catch(error => console.log(`Error occurred: ${error}`))
 
 app.listen(PORT, () => {console.log(`Server running on PORT: ${PORT}`)})
+
+// anchor class dcr-lv2v9o
